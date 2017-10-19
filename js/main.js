14ff3ad4c4fe84438f835484b9e078d0aa17c2c7
maryleo3007 <maryleo.3007@gmail.com>
2017-10-18 23:43:21 -0500

    re-design slide

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-// var closealert = document.getElementById('closeAlert');
-// closealert.addEventListener("click", function () {
-//   var alertTopClose = document.getElementById("alertTopClose");
-//   alertTopClose.classList.add("closeAlertTop");
-// });
+$(function ()  {
+  var myform = $("form#contactForm");
+  myform.submit(function(event){
+    event.preventDefault();
+    //
+    var service_id = "default_service";
+    var template_id = "template_7YYYcFVF";
+    myform.find("button").text("Sending...");
+    emailjs.sendForm(service_id,template_id,"myform")
+    .then(function(){
+      alert("Sent!");
+      myform.find("button").text("Send");
+    }, function(err) {
+      alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+      myform.find("button").text("Send");
+    });
+    return false;
+  });
+
+});
--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,33 @@
-$(_ => {
+$(function ()  {
   $('#show-nav').on('click', _ => {
     $('nav').toggleClass('show');
   });
-
   $('.contenedor').on('click', _ => {$('nav').removeClass('show');});
   $('#about').on('click', _ => {$('nav').removeClass('show');});
   $('#portafolio').on('click', _ => {$('nav').removeClass('show');});
   $('#contact').on('click', _ => {$('nav').removeClass('show');});
+
+  var lastPosition = 0;
+  window.addEventListener('scroll',function (e) {
+    e.preventDefault();
+    var posScrollY = window.scrollY;
+    if (posScrollY>0 && posScrollY<570) {
+      $('.menu-lg li:nth-child(1) a').addClass('item-select');
+      $('.menu-lg li:nth-child(2) a').removeClass('item-select');
+    }
+    if (posScrollY>570 && posScrollY<1970) {
+      $('.menu-lg li:nth-child(1) a').removeClass('item-select');
+      $('.menu-lg li:nth-child(2) a').addClass('item-select');
+      $('.menu-lg li:nth-child(3) a').removeClass('item-select');
+    }
+    if (posScrollY>1970 && posScrollY<2960) {
+      $('.menu-lg li:nth-child(2) a').removeClass('item-select');
+      $('.menu-lg li:nth-child(3) a').addClass('item-select');
+      $('.menu-lg li:nth-child(4) a').removeClass('item-select');
+    }
+    if (posScrollY>2960) {
+      $('.menu-lg li:nth-child(3) a').removeClass('item-select');
+      $('.menu-lg li:nth-child(4) a').addClass('item-select');
+    }
+  });
 });
